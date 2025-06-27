a97ef4ca5b25f5a6a54c37df34cdbf28b76d4c11
Murilo Pereira <murilo.antas@gmail.com>
2025-06-27 16:36:40 -0300

    fix omnisharp config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,20 +20,8 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({ capabilities = capabilities })
             lspconfig.omnisharp.setup({
-
                 cmd = { "omnisharp" }, -- Mason installs OmniSharp with this name by default
                 root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj"),
-                on_attach = function(client, bufnr)
-                    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-                    buf_set_option("omnisharp", "omnisharp")
-
-                    -- Optional: Custom keybindings for LSP commands
-                    -- local opts = { noremap = true, silent = true }
-                    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-                    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-                    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-                    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-                end,
                 capabilities = capabilities
             })
 
