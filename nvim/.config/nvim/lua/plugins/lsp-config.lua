97bffe8f675102e94e1361b5c8c0470abc1dc8d1
Murilo Pereira <murilo.antas@gmail.com>
2026-03-22 23:57:16 -0300

    fix require lspconfig

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -16,14 +16,25 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local omnisharp_root_dir = function(fname)
+                local root = vim.fs.root(fname, function(name)
+                    return name:match("%.sln$") ~= nil or name:match("%.csproj$") ~= nil
+                end)
 
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.omnisharp.setup({
-                cmd = { "omnisharp" }, -- Mason installs OmniSharp with this name by default
-                root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj"),
-                capabilities = capabilities
+                return root or vim.fs.root(fname, ".git")
+            end
+
+            vim.lsp.config("lua_ls", {
+                capabilities = capabilities,
             })
+            vim.lsp.enable("lua_ls")
+
+            vim.lsp.config("omnisharp", {
+                cmd = { "omnisharp" }, -- Mason installs OmniSharp with this name by default
+                root_dir = omnisharp_root_dir,
+                capabilities = capabilities,
+            })
+            vim.lsp.enable("omnisharp")
 
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
